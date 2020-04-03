97b2837f2d20f6e89c274b32645d07c7baee3abf
VictorLeFourn2 <victor.le-fourn@student.ecp.fr>
2020-04-03 17:00:07 +0200

    FIX : Louvain

--- pipeline/run_aspect_detection.py
+++ pipeline/run_aspect_detection.py
@@ -8,15 +8,21 @@ from aspect_detection import data_preprocessing, LDA
 import model
 import constants
 
-os.chdir("./pipeline")
+# os.chdir("./pipeline")
+
 
 def process(df):
-    df, texts, id2word, corpus = data_preprocessing(df, no_below=2, no_above=0.8, split_sentences=False)
+    df, texts, id2word, corpus = data_preprocessing(
+        df, no_below=2, no_above=0.8, split_sentences=False
+    )
     df = LDA(df, texts, corpus, id2word, 5)
-        
+
+
 for file in glob.glob("{}*.pkl".format(constants.PREPROCESSED_DATA_PATH)):
     df = pd.read_pickle(file)
     process(df)
-    output_path = constants.PROCESSED_DATA_PATH + '_'.join((file.split('.')[-2]).split('/')[-1].split('_')[:-1]) + '_ad.pkl'
+    output_path = (
+        constants.PROCESSED_DATA_PATH + (file.split("\\")[1]).split(".")[0] + "_ad.pkl"
+    )
     print(df)
     df.to_pickle(output_path)
