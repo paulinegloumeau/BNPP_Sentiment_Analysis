6bddc803f68b053af203ae4cd7b4897f921e1d02
VictorLeFourn2 <victor.le-fourn@student.ecp.fr>
2020-04-06 13:57:43 +0200

    Wordclouds avec clusters

--- pipeline/run_aspect_detection_louvain.py
+++ pipeline/run_aspect_detection_louvain.py
@@ -4,7 +4,8 @@ import numpy as np
 import argparse
 from tqdm import tqdm
 import glob
-
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
 
 from aspect_detection_louvain import process_aspect_detection
 import model
@@ -17,8 +18,23 @@ for file in glob.glob("{}*.pkl".format(constants.PREPROCESSED_DATA_PATH)):
     df = pd.read_pickle(file)
     clusters, new_df = process_aspect_detection(df, 50)
     print("Il y a " + str(len(clusters)) + " clusters")
-    print(clusters)
+    for i, cluster in enumerate(clusters):
+        words_in_cluster = " ".join(cluster)
+        word_cloud = WordCloud().generate(words_in_cluster)
+        plt.figure()
+        plt.imshow(word_cloud, interpolation="bilinear")
+        plt.savefig(
+            "Clusters Louvain/"
+            + (file.split("\\")[1]).split(".")[0]
+            + "_cluster_"
+            + str(i)
+        )
+        plt.axis("off")
+        plt.show()
     output_path = (
-        constants.PROCESSED_DATA_PATH + (file.split("\\")[1]).split(".")[0] + "_ad.pkl"
+        constants.PROCESSED_DATA_PATH
+        + (file.split("\\")[1]).split(".")[0]
+        + "_ad_louvain.pkl"
     )
+
     df.to_pickle(output_path)
